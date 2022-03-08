fa68a1e7ebb802f614a4e16ba42091da70f7e489
Philip Levy <philipdlevy@gmail.com>
2022-03-08 14:44:44 -0700

    commit tree da8487a1889a6b2c06a3bb27ce2da1e9b29a6adb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json", 
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(res => res.json())
+    .then( obj => {
+       //console.log(obj);
+        const body = document.querySelector('body');
+        body.innerHTML += `<h3>${obj.id}</h3>`
+    })
+    .catch( error => {
+        console.log(error)
+        //const body = document.querySelector('body');
+        document.body.innerHTML = error.message
+     })
+};
